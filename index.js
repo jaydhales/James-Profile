79928be10e91809294b71ddb6e9522c3ef28b095
James Osunkiyesi <jaydhales@gmail.com>
2022-09-12 13:23:01 +0100

    major commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const projectTab = document.querySelector("#projects-container");
+const year = document.querySelector("#year");
 const projects = [
   {
     title: "James Website",
@@ -8,48 +10,50 @@ const projects = [
   {
     title: "Unsplash Clone",
     url: "https://unsplash-clone-nu.vercel.app/",
-    snapshot: "",
+    snapshot: "./assets/unsplash.png",
     desc: "",
   },
   {
     title: "Space Tourism Website",
     url: "https://space-tourism-website-eight.vercel.app/",
-    snapshot: "",
+    snapshot: "./assets/space.png",
     desc: "",
   },
   {
     title: "Countries Stats",
     url: "https://rest-countries-api-roan.vercel.app/",
-    snapshot: "",
+    snapshot: "./assets/country.png",
     desc: "",
   },
   {
     title: "E-Commerce Page",
     url: "https://e-commerce-page-ten.vercel.app/",
-    snapshot: "",
+    snapshot: "./assets/e-comm.png",
     desc: "",
   },
   {
     title: "Ordel Landing Page",
     url: "https://ordel-project.vercel.app/",
-    snapshot: "",
+    snapshot: "./assets/ordel.png",
     desc: "",
   },
   {
     title: "Crowdfunding page",
     url: "https://crowdfunding-product-page-ruddy.vercel.app/",
-    snapshot: "",
+    snapshot: "./assets/crowdfunding.png",
     desc: "",
   },
 ];
-
-const projectTab = document.querySelector("#projects-container");
 
 projects.map(
   ({ title, url, snapshot, desc }) =>
     (projectTab.innerHTML += `<a href=${url} target="_blank" class='projects'>
     <img src=${snapshot} alt=${title} />
+    <div>
     <h4>${title}</h4>
     <p>${desc}</p>
+    </div>
 </a>`)
 );
+
+year.innerHTML = new Date().getFullYear();
